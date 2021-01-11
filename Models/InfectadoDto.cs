1881a8549a9bb508be906c5c493ee618c249c244
rodrigo33 <rbarbosa.barbosa@gmail.com>
2021-01-11 11:15:05 -0300

    aplicando model, controller, endpoint

---
+++ Models/InfectadoDto.cs
@@ -0,0 +1,10 @@
+namespace api.Models
+{
+    public class InfectadoDto
+    {
+        public DateTime dataNascimento { get; set;}
+        public string Sexo { get; set;}
+        public double Latitude { get; set;}
+        public double Longitude { get; set;}        
+    }
+}
